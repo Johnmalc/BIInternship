ce4f092da9607cacc2517266e99f255856a9c983
malcjohn <cincenko@seznam.cz>
2014-06-07 20:30:40 +0200

    test

--- R Scripts/ShipmentsPerCountryAndYear/server.R
+++ R Scripts/ShipmentsPerCountryAndYear/server.R
@@ -17,7 +17,8 @@ shinyServer(function(input, output) {
   output$gv <- renderGvis({
     myData <- subset(data, (Year > (myYear()-1)) & (Year < (myYear()+1)))
     gvisGeoChart(myData, "Customer_Country", "Count", 
-                 options=list(width=1000, height=500, colors="['#f5dcba','#4d8086','#6f6c5c', '#d6421b', '#3f6d97', '#9a6e80', '#fbb638', '#5b968e']"))
+        options=list(width=1000, height=500, 
+        colors="['#f5dcba','#4d8086','#6f6c5c', '#d6421b', '#3f6d97', '#9a6e80', '#fbb638', '#5b968e']"))
 
   })
   
