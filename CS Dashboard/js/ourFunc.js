e78b462b00a28472ae35a5746261cc4c3ef81a85
John Malc <cincenko@outlook.com>
2014-07-04 16:33:00 +0200

    Update ourFunc.js

--- CS Dashboard/js/ourFunc.js
+++ CS Dashboard/js/ourFunc.js
@@ -1,63 +1,3 @@
-/*
-function ocr(id) {  
-
-    function OCRImage(image){
-        var canvas = document.createElement('canvas')
-        canvas.width = image.naturalWidth;
-        canvas.height = image.naturalHeight;
-        canvas.getContext('2d').drawImage(image, 0, 0)
-        return OCRAD(canvas)
-    }
-
-    function OCRPath(url, callback){
-        var image = new Image()
-        image.src = url;
-        image.onload = function(){ 
-            callback(OCRImage(image))
-        }
-    }
-    
-    // This part is only used for current value where the value is located at the bottom of the picture
-    // http://stackoverflow.com/questions/5873810/how-can-i-get-last-characters-of-a-string-using-javascript
-    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/replace
-    // http://coding.pressbin.com/114/Solved-appendChild-is-not-a-function/
-    OCRPath("img/sdelogv.png", function(words){
-        //console.log(words);
-        var myString = words.replace(/[0-9]+(?!.*[0-9])/, ""); // first match string with a regex
-        var StringLastChars = myString.slice(-5); // we want just last, up-to 4 characters
-        var find = "&";
-        var re = new RegExp(find, "g");
-        var Actual = StringLastChars.replace(re, "8"); // must be otherwise doesn't recognize 8
-        var numberToCompare = parseFloat(Actual.replace(',', '.')); // JS only accepts . not a comma !
-        document.getElementById(id).innerHTML = Actual;
-
-        var Budget = words.substring(1,5); // print first 3 number with ,
-        var numberToCompareWith = parseFloat(Budget.replace(',', '.')); // JS only accepts . not a comma !
-
-        if (numberToCompare < numberToCompareWith) {         
-            var thumbContainer = document.getElementsByClassName("thmbDiv");
-            var thumbnail = document.createElement("img");
-            thumbContainer[0].appendChild(thumbnail);        
-            document.getElementById(id).style.color="red";
-            thumbnail.src = "support_img/tongue.png";
-            thumbnail.style.height = "55px";
-            thumbnail.style.width = "55px";
-        } else {
-            var thumbContainer = document.getElementsByClassName("thmbDiv");
-            var thumbnail = document.createElement("img");
-            document.getElementById(id).style.color="green";
-            thumbContainer[0].appendChild(thumbnail);
-            thumbnail.src = "support_img/pizza.png";
-            thumbnail.style.height = "55px";
-            thumbnail.style.width = "90px";
-        };
-
-        return console.log(Actual);
-    });
-
-    return true;
-}
-*/
 function ocr2(id) {  
 
     function OCRImage(image){
@@ -95,15 +35,15 @@ function ocr2(id) {
         if (numberToCompare < numberToCompareWith) {         
             var thumbContainer = document.getElementsByClassName("thmbDiv");
             var thumbnail = document.createElement("img");
-            thumbContainer[0].appendChild(thumbnail);        
-            document.getElementById(id).style.color="red";
+            thumbContainer[0].appendChild(thumbnail);                
+            //document.getElementById(id).style.color="red";
             thumbnail.src = "support_img/tongue.png";
             thumbnail.style.height = "55px";
             thumbnail.style.width = "55px";
         } else {
             var thumbContainer = document.getElementsByClassName("thmbDiv");
             var thumbnail = document.createElement("img");
-            document.getElementById(id).style.color="green";
+            //document.getElementById(id).style.color="green";
             thumbContainer[0].appendChild(thumbnail);
             thumbnail.src = "support_img/pizza.png";
             thumbnail.style.height = "55px";
@@ -132,6 +72,7 @@ function date_time(id) {
     var today = curHour + ":" + curMinute + ":" + curSeconds + " " + dayOfWeek + " " + dayOfMonth + " of " + curMonth + ", " +curYear;
     // " " +  curMeridiem +
     document.getElementById(id).innerHTML = today;
+    document.getElementById(id).style.color="black";
     setTimeout('date_time("'+id+'");','1000');
     return true;
 }
