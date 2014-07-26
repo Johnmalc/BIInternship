f17c0b6804afd9d4a9182291fd0d447f77676847
malcjohn <cincenko@seznam.cz>
2014-07-26 22:43:48 +0200

    js are now local files, instead of being on CDN update to date version with current dashboard

--- CS Dashboard/js/AjaxCallsCSV.js
+++ CS Dashboard/js/AjaxCallsCSV.js
@@ -9,11 +9,11 @@ function doAJAXRevenue(url) {
         // Store our values because after refresh these may go missing (displayed as blank)
         $("body").data("revenue", csvAsArray[0]);        
         var StoredRevenue = $("body").data("revenue");
-        $(".revenue").html(StoredRevenue).css("color", "black");
+        $(".revenue").html("(Current: " + StoredRevenue + " /").css("color", "black");
 
         $("body").data("budget", csvAsArray[2]);        
         var StoredBudget = $("body").data("budget");
-        $(".budget").html(StoredBudget).css("color", "black");
+        $(".budget").html(" &nbsp;" + StoredBudget + ")").css("color", "black");
 
         var changingVar = csvAsArray[1].toString();        
         if (changingVar.contains("-")) {
@@ -25,7 +25,7 @@ function doAJAXRevenue(url) {
         } else{
             var stringOver = "Over: ";
             $(".TextToDisplay").html(stringOver + " " + changingVar).css("color", "black");                
-            var thumbnail = $("#ocr2 .thmbDiv").height("55px").width("90px").attr('src', "support_img/pizza.png");
+            var thumbnail = $("#ocr2 .thmbDiv").height("55px").attr('src', "support_img/pizza.png");
         }
     });
 }
@@ -48,21 +48,23 @@ function playSound() {
 // Thanks to http://stackoverflow.com/questions/1531093/how-to-get-current-date-in-javascript
 function date_time(id) {
     var objToday = new Date(),
-        weekday = new Array('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday' ),
+        weekday = new Array('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Sasturday' ),
         dayOfWeek = weekday[objToday.getDay()],
         domEnder = new Array( 'th', 'st', 'nd', 'rd', 'th', 'th', 'th', 'th', 'th', 'th' ),
         dayOfMonth = today + (objToday.getDate() < 10) ? '0' + objToday.getDate() + domEnder[objToday.getDate()] : objToday.getDate() + domEnder[parseFloat(("" + objToday.getDate()).substr(("" + objToday.getDate()).length - 1))],
         months = new Array('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'),
         curMonth = months[objToday.getMonth()],
-        curYear = objToday.getFullYear(),
+        //curYear = objToday.getFullYear(),
         curHour = objToday.getHours() > 12 ? objToday.getHours() : (objToday.getHours() < 10 ? "0" + objToday.getHours() : objToday.getHours()),
         curMinute = objToday.getMinutes() < 10 ? "0" + objToday.getMinutes() : objToday.getMinutes(),
         curSeconds = objToday.getSeconds() < 10 ? "0" + objToday.getSeconds() : objToday.getSeconds(); //, 
         //curMeridiem = objToday.getHours() > 12 ? "PM" : "AM";
-    var today = curHour + ":" + curMinute + ":" + curSeconds + " " + dayOfWeek + " " + dayOfMonth + " of " + curMonth + ", " +curYear;
-    // " " +  curMeridiem +
+    var today = curHour + ":" + curMinute + ":" + curSeconds + " " + dayOfWeek + " " + dayOfMonth + " of " + curMonth; 
+    //+ ", " +curYear;
+    // " " +  curMeridiem +      
     $(".time").html(today);
     $(".time").css("color", "black");
     setTimeout('date_time("'+id+'");','1000');
     return true;
 }
+
